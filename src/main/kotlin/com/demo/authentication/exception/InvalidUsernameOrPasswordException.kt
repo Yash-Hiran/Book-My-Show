6b5626cb525487e2e3cd0ba00827203d26cf2efe
Mihir Kayastha <mihir.kayastha@medly.com>
2021-08-22 17:40:39 +0530

    feat: Added authentication api

---
+++ src/main/kotlin/com/demo/authentication/exception/InvalidUsernameOrPasswordException.kt
@@ -0,0 +1,5 @@
+package com.demo.authentication.exception
+
+import java.lang.RuntimeException
+
+class InvalidUsernameOrPasswordException : RuntimeException("Error: Invalid username or password")
---
+++ src/test/kotlin/com/demo/authentication/api/AuthenticationApiTest.kt
@@ -0,0 +1,48 @@
+package com.demo.authentication.api
+
+import com.demo.authentication.exception.InvalidUsernameOrPasswordException
+import com.demo.authentication.userCredentials.request.CredentialRequest
+import com.demo.authentication.userCredentials.service.AuthenticationService
+import com.demo.book.BaseIntegrationSpec
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.micronaut.http.HttpStatus
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class AuthenticationApiTest : BaseIntegrationSpec() {
+    private val mockAuthenticationService = mockk<AuthenticationService>()
+    private val authenticationApi = AuthenticationApi(mockAuthenticationService)
+
+    init {
+        "should successfully authenticate with valid credentials" {
+            // given
+            val credentials = CredentialRequest("mihir", "foobar")
+            every { mockAuthenticationService.checkCredentials(any()) } returns true
+            val authenticationApi = AuthenticationApi(mockAuthenticationService)
+
+            // when
+            val response = authenticationApi.login(credentials)
+
+            // then
+            verify(exactly = 1) { mockAuthenticationService.checkCredentials(credentials) }
+            response.status shouldBe HttpStatus.OK
+            response.body.get() shouldBe true
+        }
+
+        "should fail to authenticate with invalid credentials" {
+            // given
+            val credentials = CredentialRequest("mihir", "bar")
+            every { mockAuthenticationService.checkCredentials(any()) } returns false
+
+            // when
+            val exception = shouldThrow<InvalidUsernameOrPasswordException> { authenticationApi.login(credentials) }
+
+            // then
+            verify(exactly = 1) { mockAuthenticationService.checkCredentials(credentials) }
+            exception shouldBe InvalidUsernameOrPasswordException()
+            exception.message shouldBe "Error: Invalid username or password"
+        }
+    }
+}
