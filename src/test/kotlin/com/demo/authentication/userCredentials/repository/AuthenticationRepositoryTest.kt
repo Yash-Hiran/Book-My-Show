fa4df67265e32b36537fd8c8cf88159ac8323a5d
Yash-Hiran <yash.hiran@gmail.com>
2021-08-23 16:22:48 +0530

    added test for AuthenticationRepository

--- src/test/kotlin/com/demo/authentication/userCredentials/repository/AuthenticationRepositoryTest.kt
+++ src/test/kotlin/com/demo/authentication/userCredentials/repository/AuthenticationRepositoryTest.kt
@@ -1,18 +1,32 @@
 package com.demo.authentication.userCredentials.repository
 
-import com.demo.authentication.AuthenticationIntegrationSpec
 import com.demo.authentication.userCredentials.request.CredentialRequest
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import io.kotest.matchers.shouldBe
 import norm.executeCommand
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import javax.inject.Inject
+import javax.sql.DataSource
 
-class AuthenticationRepositoryTest: AuthenticationIntegrationSpec() {
+@MicronautTest
+class AuthenticationRepositoryTest(@Inject private val dataSource: DataSource) : StringSpec() {
     private val authenticationRepository = AuthenticationRepository(dataSource)
+    override fun afterEach(testCase: TestCase, result: TestResult) {
+        super.afterEach(testCase, result)
+        clearData()
+    }
+    private fun clearData() = dataSource.connection.use {
+        it.executeCommand("TRUNCATE TABLE users RESTART IDENTITY;")
+    }
 
     init {
+        addPgcrypto()
         "should return true for correct credentials" {
             // given
             val credentials = CredentialRequest("mihir", "12345")
-            createUser("mihir", "12345")
+            createUser()
 
             // when
             val result = authenticationRepository.checkCredentials(credentials)
@@ -24,7 +38,7 @@ class AuthenticationRepositoryTest: AuthenticationIntegrationSpec() {
         "should return false for incorrect credentials" {
             // given
             val credentials = CredentialRequest("raj", "xyz")
-            createUser("yash", "asdf")
+            createUser()
 
             // when
             val result = authenticationRepository.checkCredentials(credentials)
@@ -34,11 +48,11 @@ class AuthenticationRepositoryTest: AuthenticationIntegrationSpec() {
         }
     }
 
-    private fun  createUser(username: String, password: String) = dataSource.connection.use {
-        it.executeCommand("""
-            |INSERT INTO users(username, password)
-            |VALUES ($username, CRYPT($password, GEN_SALT('bf')));
-            |""".trimMargin().trimIndent()
-        )
+    private fun addPgcrypto() = dataSource.connection.use {
+        it.executeCommand("CREATE EXTENSION pgcrypto;")
+    }
+
+    private fun createUser() = dataSource.connection.use {
+        it.executeCommand("""insert into users (username, password) values ('mihir', CRYPT('12345', gen_salt('bf')));""")
     }
 }
