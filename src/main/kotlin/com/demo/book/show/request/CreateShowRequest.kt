de39b31c35c7656f9b647915338d709dc5d20880
AnshikaGarg01 <88761682+AnshikaGarg01@users.noreply.github.com>
2021-08-23 11:27:38 +0530

    Added Show API Test

--- src/main/kotlin/com/demo/book/show/request/CreateShowRequest.kt
+++ src/main/kotlin/com/demo/book/show/request/CreateShowRequest.kt
@@ -3,5 +3,6 @@ package com.demo.book.movie.request
 import com.demo.book.movie.entity.Movie
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 data class CreateShowRequest(val movieId : Int, val date : LocalDate, val startTime : LocalDateTime, val endTime : LocalDateTime)
--- src/test/kotlin/com/demo/book/api/ShowApiTest.kt
+++ src/test/kotlin/com/demo/book/api/ShowApiTest.kt
@@ -1,15 +1,22 @@
 package com.demo.book.api
 
+
 import com.demo.book.BaseIntegrationSpec
 import com.demo.book.movie.entity.Movie
 import com.demo.book.movie.request.CreateMovieRequest
+import com.demo.book.movie.request.CreateShowRequest
+import com.demo.book.show.entity.Show
 import com.demo.book.utils.get
 import com.demo.book.utils.post
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.extensions.time.withConstantNow
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.exceptions.HttpClientResponseException
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 class ShowApiTest : BaseIntegrationSpec() {
 
@@ -17,9 +24,35 @@ class ShowApiTest : BaseIntegrationSpec() {
 
         "should get all saved shows" {
             // When
+            createNewShow(newShowRequest())
+            val response = httpClient.get<List<Show>>("/shows")
 
+            // Then
+            response.status shouldBe HttpStatus.OK
+            val savedShows = response.body.get()
+            savedShows.size shouldBe 1
+            jsonString(savedShows[0]) shouldBe """
+                |{
+                |  "id" : 1,
+                |  "title" : "Avengers",
+                |  "duration" : 100
+                |}
+            """.trimMargin().trimIndent()
 
         }
+        
+    }
+    private fun createNewShow(show: CreateShowRequest): HttpResponse<Any> {
+        return httpClient.post(
+            url = "/shows",
+            body = jsonMapper.writeValueAsString(show)
+        )
+    }
+
+    private fun newShowRequest(): CreateShowRequest {
+        return CreateShowRequest(
+             1, LocalDate.now() , LocalDateTime.now() ,  LocalDateTime.now()
+        )
     }
 
 }
