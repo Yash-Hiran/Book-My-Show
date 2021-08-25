201900317b5cb92981b16b0ac8f331db11e044a2
Yash-Hiran <yash.hiran@gmail.com>
2021-08-25 19:49:06 +0530

    refactor(HttpUtils): changed methods to use basic authorization

---
+++ src/test/kotlin/com/demo/utils/HttpUtils.kt
@@ -0,0 +1,29 @@
+package com.demo.utils
+
+import com.demo.authentication.userCredentials.UserCredentialsRequest
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+
+inline fun <reified T> HttpClient.getWithBasicAuth(
+    url: String,
+    userCredentialsRequest: UserCredentialsRequest
+) = toBlocking().exchange(
+    HttpRequest.GET<T>(url).basicAuth(
+        userCredentialsRequest.username,
+        userCredentialsRequest.password
+    ),
+    T::class.java
+)
+
+inline fun <reified T> HttpClient.postWithBasicAuth(
+    url: String,
+    body: T,
+    userCredentialsRequest: UserCredentialsRequest
+) = toBlocking()
+    .exchange(
+        HttpRequest.POST(url, body).basicAuth(
+            userCredentialsRequest.username,
+            userCredentialsRequest.password
+        ),
+        T::class.java
+    )
