d326b4b1eaab7883b2038c653958a4e77aa38255
Yash-Hiran <yash.hiran@gmail.com>
2021-08-25 19:50:48 +0530

    refactor(MovieApi): added authentication restriction

--- src/main/kotlin/com/demo/book/api/MovieApi.kt
+++ src/main/kotlin/com/demo/book/api/MovieApi.kt
@@ -1,33 +1,24 @@
 package com.demo.book.api
 
-import com.demo.authentication.exception.InvalidUsernameOrPasswordException
-import com.demo.authentication.userCredentials.repository.AuthenticationRepository
-import com.demo.authentication.userCredentials.service.AuthenticationService
 import com.demo.book.movie.entity.Movie
 import com.demo.book.movie.request.CreateMovieRequest
 import com.demo.book.movie.service.MovieService
-import io.micronaut.http.BasicAuth
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.*
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import javax.inject.Inject
-import javax.sql.DataSource
 
 @Controller
-class MovieApi(@Inject val movieService: MovieService, @Inject private val dataSource: DataSource) {
-
+@Secured(SecurityRule.IS_AUTHENTICATED)
+class MovieApi(@Inject private val movieService: MovieService) {
     @Get("/movies")
     fun allMovies(): HttpResponse<List<Movie>> {
         return HttpResponse.ok(movieService.allMovies())
     }
 
     @Post("/movies")
-    fun saveMovie(
-        @Header basicAuth: BasicAuth,
-        @Body movieRequest: CreateMovieRequest
-    ): HttpResponse<Int> {
-        if (AuthenticationService(AuthenticationRepository(dataSource))
-                .checkCredentials(basicAuth)
-        ) return HttpResponse.ok(movieService.save(movieRequest).id)
-        else throw InvalidUsernameOrPasswordException("com.movie.api.service")
+    fun saveMovie(@Body movieRequest: CreateMovieRequest): HttpResponse<Int> {
+        return HttpResponse.ok(movieService.save(movieRequest).id)
     }
 }
--- src/test/kotlin/com/demo/book/api/MovieApiTest.kt
+++ src/test/kotlin/com/demo/book/api/MovieApiTest.kt
@@ -1,20 +1,13 @@
 package com.demo.book.api
 
-import com.demo.authentication.exception.InvalidUsernameOrPasswordException
-import com.demo.authentication.userCredentials.repository.AuthenticationRepository
-import com.demo.authentication.userCredentials.service.AuthenticationService
+import com.demo.authentication.userCredentials.UserCredentialsRequest
 import com.demo.book.BaseIntegrationSpec
 import com.demo.book.movie.entity.Movie
-import com.demo.book.movie.repository.MovieRepository
 import com.demo.book.movie.request.CreateMovieRequest
-import com.demo.book.movie.service.MovieService
-import com.demo.book.utils.get
-import com.demo.book.utils.post
-import io.kotest.assertions.throwables.shouldThrow
+import com.demo.utils.getWithBasicAuth
+import com.demo.utils.postWithBasicAuth
 import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldBe
-import io.micronaut.http.BasicAuth
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.test.extensions.kotest.annotation.MicronautTest
 import norm.executeCommand
@@ -22,55 +15,53 @@ import javax.sql.DataSource
 
 @MicronautTest
 class MovieApiTest(override var dataSource: DataSource) : BaseIntegrationSpec() {
-    private val authenticationRepository = AuthenticationRepository(dataSource)
-    private val authenticationService = AuthenticationService(authenticationRepository)
-    private val movieRepository = MovieRepository(dataSource)
-    private val movieService = MovieService(movieRepository)
-    private val movieApi = MovieApi(movieService, dataSource)
 
     override fun beforeEach(testCase: TestCase) {
         super.beforeEach(testCase)
         clearData()
     }
 
-    override fun clearData(): Unit = dataSource.connection.use{
+    override fun clearData(): Unit = dataSource.connection.use {
         it.executeCommand("TRUNCATE TABLE users RESTART IDENTITY CASCADE;")
         it.executeCommand("TRUNCATE TABLE movies RESTART IDENTITY CASCADE;")
     }
 
-    private fun createUser(basicAuth: BasicAuth) = dataSource.connection.use {
+    private fun createUser(userCredentialsRequest: UserCredentialsRequest) = dataSource.connection.use {
         it.executeCommand(
             """
             | INSERT INTO users (username, password)
-            | VALUES ('${basicAuth.username}', CRYPT('${basicAuth.password}', GEN_SALT('bf')));
+            | VALUES ('${userCredentialsRequest.username}', CRYPT('${userCredentialsRequest.password}', GEN_SALT('bf')));
             | """.trimMargin().trimIndent()
         )
     }
 
     init {
+        val adminCredentials = UserCredentialsRequest("Yash", "Mihir")
+        val nonAdminCredentials = UserCredentialsRequest("Mihir", "Yash")
+
         "should save movie with correct credentials" {
             // Given
-            createUser(BasicAuth("kraken", "ryu"))
-            val credentials = BasicAuth("kraken", "ryu")
+            createUser(adminCredentials)
+            val requestCredentials = adminCredentials
 
             // When
-            val response = createNewMovie(newMovieRequest(100), credentials)
+            val response = createNewMovie(newMovieRequest(100), requestCredentials)
 
             // Then
             response.status shouldBe HttpStatus.OK
-            response.body.get() shouldBe 1
+            response.body.get() shouldBe "1"
         }
 
-        "should get all saved movies" {
+        "should get all saved movies with correct credentials" {
+            // Given
+            createUser(adminCredentials)
+
             // When
-            val credentials = BasicAuth("kraken", "ryu")
-            createUser(credentials)
-            createNewMovie(newMovieRequest(100), credentials)
-            val response = httpClient.get<List<Movie>>("/movies")
+            val response = createNewMovie(newMovieRequest(100), adminCredentials)
 
             // Then
             response.status shouldBe HttpStatus.OK
-            val savedMovies = response.body.get()
+            val savedMovies = getAllMoviesWithAuth(adminCredentials).body.get()
             savedMovies.size shouldBe 1
             jsonString(savedMovies[0]) shouldBe """
                 |{
@@ -81,40 +72,51 @@ class MovieApiTest(override var dataSource: DataSource) : BaseIntegrationSpec() 
             """.trimMargin().trimIndent()
         }
 
-        "should get empty list if no movies are present" {
-            // When
-            val response = httpClient.get<List<Movie>>("/movies")
-
-            // Then
-            response.status shouldBe HttpStatus.OK
-            val savedMovies = response.body.get()
-            savedMovies.size shouldBe 0
-            jsonString(savedMovies) shouldBe "[ ]"
-        }
-
-        "should throw exception when saving a movie with incorrect credentials" {
+        "should get empty list if no movies are present with correct credentials" {
             // Given
-            createUser(BasicAuth("kraken", "ryu"))
-            val credentials = BasicAuth("blackbeard", "nio")
-
-            // When
-            val exception = shouldThrow<InvalidUsernameOrPasswordException>
-            { createNewMovie(newMovieRequest(100), credentials) }
+            createUser(adminCredentials)
 
             // Then
-            exception.code shouldBe "com.authentication.service"
-            exception.message shouldBe "Invalid username or password"
+            val savedMovies = getAllMoviesWithAuth(adminCredentials).body.get()
+            savedMovies.size shouldBe 0
+            jsonString(savedMovies[0]) shouldBe "[ ]"
+        }
+
+        "should respond with bad request when saving a movie with incorrect credentials" {
+            // When
+            val response = createNewMovie(newMovieRequest(100), nonAdminCredentials)
+
+            // Then
+            response.status shouldBe HttpStatus.BAD_REQUEST
+        }
+
+        "should respond with bad request when getting movie with incorrect credentials" {
+            // When
+            val response = getAllMoviesWithAuth(nonAdminCredentials)
+
+            // Then
+            response.status shouldBe HttpStatus.BAD_REQUEST
         }
     }
 
-    private fun createNewMovie(avengersMovie: CreateMovieRequest, basicAuth: BasicAuth): HttpResponse<Int> {
-        return movieApi.saveMovie(basicAuth, avengersMovie)
-    }
+    private fun getAllMoviesWithAuth(userCredentialsRequest: UserCredentialsRequest) =
+        httpClient.getWithBasicAuth<List<Movie>>(
+            url = "/movies",
+            userCredentialsRequest = userCredentialsRequest
+        )
 
-    private fun newMovieRequest(duration: Int): CreateMovieRequest {
-        return CreateMovieRequest(
+    private fun createNewMovie(
+        avengersMovie: CreateMovieRequest,
+        userCredentialsRequest: UserCredentialsRequest
+    ) = httpClient.postWithBasicAuth(
+        url = "/movies",
+        body = jsonMapper.writeValueAsString(avengersMovie),
+        userCredentialsRequest = userCredentialsRequest
+    )
+
+    private fun newMovieRequest(duration: Int) =
+        CreateMovieRequest(
             "Avengers",
             duration
         )
-    }
 }
