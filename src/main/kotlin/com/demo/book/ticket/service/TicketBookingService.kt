90ef71abee83f902a0e9c57ba75160884b6028bd
Mihir Kayastha <mihir.kayastha@medly.com>
2021-08-27 12:50:29 +0530

    feat(TicketBooking): created api to book tickets

--- src/main/kotlin/com/demo/book/ticket/service/TicketBookingService.kt
+++ src/main/kotlin/com/demo/book/ticket/service/TicketBookingService.kt
@@ -1,6 +1,5 @@
 package com.demo.book.ticket.service
 
-import com.demo.book.ticket.entity.Ticket
 import com.demo.book.ticket.repository.TicketBookingRepository
 import com.demo.book.ticket.request.TicketRequest
 import javax.inject.Inject
